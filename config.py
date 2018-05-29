0d669fa0c04defc7c6d6c20d51f34ed310aa6451
murungi kirima <murungikirima@gmail.com>
2018-05-29 18:20:01 +0300

    new migration

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     CSRF_INSTALLED = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'postgresql+psycopg2://murungi:murungi1@localhost/plog')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql+psycopg2://murungi:murungi1@localhost/plog')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY="svcdsdhgcvghsg"
 
@@ -10,7 +10,7 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'postgresql+psycopg2://murungi:murungi1@localhost/plog')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql+psycopg2://murungi:murungi1@localhost/plog')
     DEBUG = True
 
 class TestingConfig(Config):
